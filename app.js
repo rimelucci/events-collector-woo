969fc49e69d6f60860a395bffc2d5d7ca4b4210d
JeffreyZou13 <jeffreyzou13@gmail.com>
2015-12-10 22:57:44 -0500

    played with data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var city, artist;
+var name, types, dates, time;
 
-$("button").click(function(){
+$("#submit").click(function(){
   city = $("#city-input").text();
   artist = $("#artist-input").text();
   formatedCity = city.split(" ");
@@ -12,23 +13,26 @@ $("button").click(function(){
     for (word in formatedCity) {
       link = link + word + "+";
     }
-  };
-  if (artist !=== "") {
-    link = link + "&artist_name" +
   }
-  var storage = []; // list of events and all the data
-  var name = []; //Names of the events
-  var artists = []; //Uris for artists' profiles
+  if (artist !== "") {
+    link = link + "&artist_name";
+    for (word in formatedName) {
+      link = link + word + "+";
+    }
+  }
+
+  name = []; //Names of the events
+  types = []; //types e.g. concert
+  dates = [];
+  time = [];
   $.getJSON(link, function(data) {
     $.each(storage.append(data["resultsPage"]["results"]["event"]));
-    for (i in storage) {
-      name.append(i['displayName'])
+    for (i=0;i<storage.length;i++) {
+      name.append(storage[i]['displayName']);
+      types.append(storage[i]['type']);
+      dates.append(storage[i]['start']['date']);
+      time.append(storage[i]['start']['time']);
     }
-    for (event in storage) {
-      for (i=0;i<event.length;i++) {
-        artists.append(event[i]['artist']['uri']);
-        //We don't necessarily need all of the variables
-      }
-    }
+    //do stuff with the data
   });
 });
